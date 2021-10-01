3f4f9c35001335ad0d02e2ffffab4d7581421cbf
PanosGriziotis <72866911+PanosGriziotis@users.noreply.github.com>
2021-10-01 19:39:56 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+from final_project import FrequenciesGenerator as FG
+
+test = FG('test_data_Μ901_final_project')
+test.read_folder()
+print(test.get_frequency('αλλά', '35406.pdf.txt'))
+print(test.calculate_similarity('35406.pdf.txt', 'Handout 01. Decibels.doc.txt'))
