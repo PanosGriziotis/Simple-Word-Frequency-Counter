2a9df61f22edddc69f51b1b716be2381f38db574
PanosGriziotis <72866911+PanosGriziotis@users.noreply.github.com>
2021-10-02 17:15:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from frequency_counter import FrequenciesGenerator as FG
 
-test = FG('test_data_Μ901_final_project')
+# insert path of the folder with text files
+test = FG('test_data')
 test.read_folder()
 print(test.get_frequency('αλλά', '35406.pdf.txt'))
 print(test.calculate_similarity('35406.pdf.txt', 'Handout 01. Decibels.doc.txt'))
